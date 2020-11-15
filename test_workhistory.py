5fed083f93a191b2fcd7a877fe86705d73583ec1
casswe368 <casswe368@gmail.com>
2020-11-15 00:55:23 -0500

    Testing update

--- test_workhistory.py
+++ test_workhistory.py
@@ -5,7 +5,9 @@ Created on Sat Nov 14 22:01:55 2020
 @author: cassw
 """
 
-
+#Goal: get a list of all ids in the user's catalog of works
+#search each work for "shockwave-flash" to flag works that need to be updated after flash plug-in decommission
+#hopeful later goal: replace outdated code with new html5 version
 
 
 from src.ao3 import AO3
@@ -16,15 +18,33 @@ username=cred.username
 password=cred.password
 api.login(username, password)
 
-work = api.work(id='27194623')
-print('title')
-#print(work._html)
-print(work.title)
-
 work = api.work(id='7215418')
 print('title')
 #print(work._html)
 print(work.title)
+print('text')
 
-workList = api.user.catalog_ids()
-print(workList)
+work2 = api.work(id='25717039')
+print('title')
+#print(work2._html)
+print(work2.title)
+
+#workList = api.user.catalog_ids()
+#print(workList)
+#print(work2.summary)
+
+"""
+f = open('WorkHTMLTEST.txt', 'w', encoding='utf-8')
+html=work2._html
+soup=work2._soup
+#f.write('html starts here')
+#f.write(html)
+#f.write('html ends here')
+#f.write(' ')
+#f.write('soup starts here')
+f.write(soup.prettify())
+#f.write('soup ends here')
+f.close()
+"""
+
+print(work2.body)
